188f10f789e6a4fbd71224d32a42b3e06a53873f
LK4z <lk4zvpn@gmail.com>
2025-08-19 22:03:08 -0300

    sitemap add

---
+++ app/sitemap.ts
@@ -0,0 +1,29 @@
+// app/sitemap.ts
+
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = 'https://caseirinhosdaamalia.vercel.app';
+
+  return [
+    {
+      url: baseUrl, // URL da página inicial
+      lastModified: new Date(), // Data da última modificação
+      changeFrequency: 'daily', // Frequência de mudança
+      priority: 1, // Prioridade
+    },
+    {
+      url: `${baseUrl}/sobre`, // URL da página "Sobre"
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/catalogo`, // Exemplo de URL para uma página de produtos
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    // Adicione outras URLs aqui, se necessário
+  ];
+}
